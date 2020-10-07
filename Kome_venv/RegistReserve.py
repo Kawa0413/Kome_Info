c8efec8dd90355f1f5808e682e728f2a2cfa59ae
kome <unesco777@docomo.ne.jp>
2020-10-07 15:55:03 +0900

    26 commit

--- Kome_venv/RegistReserve.py
+++ Kome_venv/RegistReserve.py
@@ -7,18 +7,18 @@ import requests
 #import locale
 import json
 import os
-from os.path import join, dirname
-from python-dotenv import load_dotenv
+# from os.path import join, dirname
+# from python-dotenv import load_dotenv
 
 
 #load_envでファイルの中身を読み込む
 
-load_dotenv(verbose=True)
+# load_dotenv(verbose=True)
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-#MONGODB_URLを読み込む
-PWD = os.environ.get("MONGODB_URL")
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+# #MONGODB_URLを読み込む
+# PWD = os.environ.get("MONGODB_URL")
 
 #CORSで同一制限元ポリシーの制限緩める
 #bson.objectidでデータ更新
@@ -48,7 +48,7 @@ class Line_bot:
             
 #Flaskクラスのインスタンス作ってapp(変数)に代入
 app = Flask(__name__, static_folder='Frontend')
-client = MongoClient("PWD")
+client = MongoClient("mongodb+srv://Kome:Hikaru0721@komeinfo.mmmm6.mongodb.net/admin?authSource=admin&replicaSet=atlas-214nbh-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 #mongodb+srv://Kome:*****@komeinfo.mmmm6.mongodb.net/admin?authSource=admin&replicaSet=atlas-214nbh-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 #client = MongoClient("MONGODB_URL")mongodb://127.0.0.1:27017/Kome_Info
 db = client.Kome_Info
